045bde420baa0bc135cead741946523d78eaf1af
maulik-d9 <maulik.d9ithub@gmail.com>
2023-12-08 18:51:38 +0530

    work done of 2nd, 3rd and 4th page responsive

--- resources/js/components/ComponentFour.vue
+++ resources/js/components/ComponentFour.vue
@@ -1,34 +1,35 @@
 <template>
     <div class="bg-image-container four">
-        <div class="container-fluid pt-5">
-            <div class="row side-spacing">
-                <div class="col-4 px-0"></div>
-                <div class="col-4 px-0 text-center">
+        <div class="container-fluid">
+            <div class="row side-spacing mx-auto">
+                <div class="col-xl-4 col-md-2 d-md-block"></div>
+                <div class="col-xl-4 col-md-8 text-center">
                     <h2 class="text-center text-default text-size-default mb-4">רשימת נשאויות גנטיות</h2>
-                    <div class="w-75 mx-auto position-relative">
+                    <div class="mx-auto position-relative">
                         <input type="text" class="form-input text-default w-100" placeholder="חיפוש חופשי">
                         <img src="../../../public/images/svg/search.svg" class="search-icon" alt="img" width="100%" height="auto">
                     </div>
-                    <div class="w-75 mx-auto position-relative mt-4">
+                    <div class="mx-auto position-relative mt-4">
                         <input type="text" class="form-input text-default border-transparent w-100" placeholder="חיפוש לפי שם הנשאות">
                         <img src="../../../public/images/svg/half-border.svg" class="half-border-icon" alt="img" width="100%" height="auto">
                         <img src="../../../public/images/svg/left.svg" class="search-icon" alt="img" width="100%" height="auto" style="cursor: pointer" @click.prevent="redirectToPage('/ten')">
                     </div>
-                    <h5 class="text-center text-default mt-4 mb-2"></h5>
                 </div>
-                <div class="col-4 px-0"></div>
+                <div class="col-xl-4 col-md-2 d-md-block"></div>
             </div>
-            <div class="side-spacing pt-0">
+            <div class="row side-spacing mx-auto pt-0">
                 <div class="text-center">
                     <img src="../../../public/images/babies.png" class="babies-background pb-5" alt="img" width="100%" height="auto">
                 </div>
             </div>
-            <div class="row footer-content">
-                <div class="col-6 text-right">
-                    <img src="../../../public/images/ihilov.png" alt="img" width="100%" height="auto">
-                </div>
-                <div class="col-6">
-                    <button class="btn-default btn-outlined text-default border-white" style="float: left" @click.prevent="redirectToPage('/five')">המשך ></button>
+            <div class="footer-content">
+                <div class="row">
+                    <div class="col-sm-6 text-right">
+                        <img src="../../../public/images/ihilov.png" alt="img" width="100%" height="auto">
+                    </div>
+                    <div class="col-sm-6">
+                        <button class="btn-default btn-outlined text-default border-white" style="float: left" @click.prevent="redirectToPage('/five')">המשך ></button>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/js/components/ComponentTwo.vue
+++ resources/js/components/ComponentTwo.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="bg-image-container two">
         <div class="container-fluid pt-5">
-            <div class="row side-spacing">
-                <div class="col-5 px-0">
-                    <h4 class="text-right text-default">על מנת לדייק את התורמים המתאימים עבורך, נשמח</h4>
-                    <h4 class="text-right text-default">שתעני על השאלות הבאות:</h4>
-                    <h2 class="text-right text-default text-size-default mt-4 mb-2">CMV</h2>
+            <div class="row side-spacing mx-auto">
+                <div class="col-xl-6">
+                    <h4 class="text-default">על מנת לדייק את התורמים המתאימים עבורך,</h4>
+                    <h4 class="text-default">נשמח שתעני על השאלות הבאות:</h4>
+                    <h2 class="text-default text-size-default mt-4 mb-2">CMV</h2>
                     <div class="d-flex align-items-center">
                         <label class="capsual-button">
                             <input type="checkbox"/>
@@ -16,18 +16,18 @@
                             <div class="checked-bg">לא</div>
                         </label>
                     </div>
-                    <h2 class="text-right text-default text-size-default mt-4 mb-2">תרומת ביצית</h2>
+                    <h2 class="text-default text-size-default mt-4 mb-2">תרומת ביצית</h2>
                     <div class="d-flex align-items-center">
                         <label class="capsual-button">
                             <input type="checkbox"/>
-                            <div class="checked-bg">לא</div>
+                            <div class="checked-bg">כן</div>
                         </label>
                         <label class="capsual-button me-5">
                             <input type="checkbox"/>
-                            <div class="checked-bg">כן</div>
+                            <div class="checked-bg">לא</div>
                         </label>
                     </div>
-                    <h2 class="text-right text-default text-size-default mt-4 mb-2">נשאות גנטית</h2>
+                    <h2 class="text-default text-size-default mt-4 mb-2">נשאות גנטית</h2>
                     <div class="d-flex align-items-center">
                         <label class="capsual-button">
                             <input type="checkbox"/>
@@ -39,14 +39,16 @@
                         </label>
                     </div>
                 </div>
-                <div class="col-7 px-0"></div>
+                <div class="col-xl-6 d-none d-xl-block"></div>
             </div>
-            <div class="row footer-content">
-                <div class="col-6 text-right">
-                    <img src="../../../public/images/ihilov.png" alt="img" width="100%" height="auto">
-                </div>
-                <div class="col-6">
-                    <button class="btn-default btn-outlined text-default border-white" style="float: left" @click.prevent="redirectToPage">המשך ></button>
+            <div class="footer-content">
+                <div class="row">
+                    <div class="col-sm-6 text-right">
+                        <img src="../../../public/images/ihilov.png" alt="img" width="100%" height="auto">
+                    </div>
+                    <div class="col-sm-6">
+                        <button class="btn-default btn-outlined text-default border-white" style="float: left" @click.prevent="redirectToPage('/three')">המשך ></button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -60,8 +62,8 @@ export default {
         }
     },
     methods: {
-        redirectToPage() {
-            this.$router.push('/three');
+        redirectToPage(route) {
+            this.$router.push(route);
         }
     },
 }
